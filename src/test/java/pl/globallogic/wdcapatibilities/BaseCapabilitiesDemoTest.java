7aaeb49304afa11cec19537d5b46f1fb3c068ed6
matthewtop <tolpamateusz@gmail.com>
2023-08-31 17:12:53 +0200

    WebDriver Capabilities

---
+++ src/test/java/pl/globallogic/wdcapatibilities/BaseCapabilitiesDemoTest.java
@@ -0,0 +1,55 @@
+package pl.globallogic.wdcapatibilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseCapabilitiesDemoTest {
+    protected WebDriver driver;
+    protected String SANDBOX_ADDRESS ="https://bonigarcia.dev/selenium-webdriver-java/";
+
+    String WEB_FORM = SANDBOX_ADDRESS = "web-form.html";
+    String DROPDOWN_MENU = SANDBOX_ADDRESS = "dropdown-menu.html";
+    String DRAW_IN_CANVAS = SANDBOX_ADDRESS = "draw-in-canvas.html";
+    String LOADING_IMAGES = SANDBOX_ADDRESS = "loading-images.html";
+    String SLOW_CALCULATOR= SANDBOX_ADDRESS = "slow-calculator.html";
+
+    protected void sleep(){
+        try{
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    @BeforeClass
+    public void globalSetup(){
+        WebDriverManager.chromedriver().setup();
+        System.out.println("Starting Selenium Webdriver capabilities demonstration");
+
+    }
+
+    @BeforeMethod
+    public void setUp(){
+        driver=new ChromeDriver();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void cleanUp(){
+        driver.quit();
+    }
+
+    @AfterClass
+    public void globalCleanUp(){
+        System.out.println("Finished with web driver capabilities demonstration");
+
+    }
+
+
+
+}
---
+++ src/test/java/pl/globallogic/wdcapatibilities/DrawCanvasDemoTest.java
@@ -0,0 +1,33 @@
+package pl.globallogic.wdcapatibilities;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class DrawCanvasDemoTest extends BaseCapabilitiesDemoTest{
+
+    @Test
+    public void shouldDrawCircleInCanvas(){
+        driver.get(DRAW_IN_CANVAS);
+        Actions actions = new Actions(driver);
+        WebElement canvas = driver.findElement(By.tagName("canvas"));
+        sleep();
+        actions.moveToElement(canvas).clickAndHold();
+        drawCircle(actions);
+        actions.release(canvas).build().perform();
+
+    }
+
+    private void drawCircle(Actions actions) {
+        int numPoints = 30;
+        int radius = 5;
+        for (int i = 0; i < numPoints; i++) {
+            double angle = Math.toRadians((double) (360 * i) /numPoints);
+            double x = Math.sin(angle)*radius;
+            double y = Math.cos(angle)*radius;
+            actions.moveByOffset((int)x, (int)y);
+
+        }
+    }
+}
